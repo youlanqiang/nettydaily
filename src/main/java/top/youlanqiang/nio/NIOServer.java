dcdb6e3a95207825e40579bb4d6b571c70278881
youlanqiang <youlanqiang@hotmail.com>
2022-02-05 13:14:32 +0800

    NIOServer和NIOClient代码重写

--- src/main/java/top/youlanqiang/nio/NIOServer.java
+++ src/main/java/top/youlanqiang/nio/NIOServer.java
@@ -3,88 +3,132 @@ package top.youlanqiang.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 
 public class NIOServer {
 
 
-    public static void main(String[] args) throws IOException {
-        // 创建ServerSocketChannel -> ServerSocket
+    // 缓冲区的大小
+    private final static int BUFFER_SIZE = 1024;
+
+    // 缓冲区
+    private ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+
+    // Server监听的端口号
+    // private final static int PORT = 8888;
+
+    // 选择器
+    private Selector selector = null;
+
+    // 初始化工作
+    public void init(int port) throws IOException {
+        System.out.println("============ Listening On Port : " + port + "============");
+        // 打开服务器套接字通道
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-
-        // 得到一个Selector对象
-        Selector selector = Selector.open();
-
-        // 绑定一个端口 6666 在服务器端监听
-        serverSocketChannel.bind(new InetSocketAddress(6666));
-
-        // 设置为非阻塞模式
+        // 设置为非阻塞状态
         serverSocketChannel.configureBlocking(false);
 
-        // 将 serverSocketChannel注册到 selector 事件为OP_ACCEPT
+        // 绑定端口号
+        serverSocketChannel.bind(new InetSocketAddress(port));
+        // 打开一个选择器
+        selector = Selector.open();
+        // 服务器套接字注册到Selector中 并指定Selector监控连接事件
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+    }
 
-
-        while(true){
-
-            // 等待一秒,如果没有事件发生，就继续
-            if(selector.select(1_000) == 0){ // 没有事件发生
-//                System.out.println("服务器等待了1秒,无连接");
+    public void listen() throws IOException {
+        while (true) {
+            // 开启选择
+            int readyChannels = selector.select(); // 没有通道就绪 一直阻塞 返回已经就绪通道的数目(有可能为0)
+            if (readyChannels == 0) {
                 continue;
             }
-
-            // 如果返回>0,就获取到相关的selectionKey集合
-
-            // 返回关注事件的集合
-            // 通过selectionKeys 反向获取通道
-            Set<SelectionKey> selectionKeys = selector.selectedKeys();
-            // 迭代器遍历集合 迭代器可以安全,方便的remove元素
-            Iterator<SelectionKey> iterator = selectionKeys.iterator();
-            
-
-            while(iterator.hasNext()){
-                SelectionKey key = iterator.next();
-
-                // 手动从集合中移除当前的selectionKey,防止重复操作
+            // 返回已选择键的集合
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            // 遍历键 并检查键对应的通道里注册的就绪事件
+            Iterator iterator = selectedKeys.iterator();
+            while (iterator.hasNext()) {
+                // SelectionKey封装了一个通道和选择器的注册关系
+                SelectionKey key = (SelectionKey) iterator.next();
+                handleKey(key);
+                // Selector不会移除SelectionKey 处理完了手动移除
                 iterator.remove();
+            }
+        }
+    }
 
-                if(key.isAcceptable()){ // 如果是 OP_ACCEPT; 表示有新的客户端连接
-                    // 客户端生成一个SocketChannel
-                    SocketChannel socketChannel = serverSocketChannel.accept();
-                    // 配置为非阻塞模型
-                    socketChannel.configureBlocking(false);
-
-                    System.out.println("客户端连接成功,生成了一个 socketChannel hashcode:"+socketChannel.hashCode());
-                    // 将当前Socket注册到Selector中 关注事件为READ,同时关联一个buffer
-                    socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
-
-                }
-                if(key.isReadable()){ // 如果是 OP_READ; 可读事件
-                    //通过 key来反向获取 对应的channel
-                    SocketChannel socketChannel = (SocketChannel) key.channel();
-                    // 获取到该channel关联的buffer
-                    ByteBuffer buffer =  (ByteBuffer) key.attachment();
-                    int length = socketChannel.read(buffer);
-                    //在客户端断开连接后，出现了不断产生新OP_READ事件
-                    //原因就在于断开连接后，为了让你知道连接已断开，所以会产生OP_READ事件。
-                    //那么该怎么判断呢？
-                    //其实只要判断一下byteBuffer的大小就可以了，当byteBuffer的长度小于0时，说明连接断开了，那么把channel关闭就可以了。
-                    if(length < 0){
-                        System.out.println("客户端关闭.");
-                        socketChannel.close();
-                    }else{
-                        System.out.println("客户端发送:"+ new String(buffer.array()));
-                    }
-                }
+    // 处理SelectionKey
+    private void handleKey(SelectionKey key) throws IOException {
+        // 是否有连接进来
+        if (key.isAcceptable()) {
+            ServerSocketChannel server = (ServerSocketChannel) key.channel();// 获取通道 转化为要处理的类型
+            SocketChannel socketChannel = server.accept();
+            // SocketChannel通道的可读事件注册到Selector中
+            registerChannel(selector, socketChannel, SelectionKey.OP_READ);
+            // 连接成功 向Client打个招呼
+            if (socketChannel.isConnected()) {
+                buffer.clear();
+                buffer.put("I am Server...".getBytes());
+                buffer.flip();
+                socketChannel.write(buffer);
 
             }
+
+        }
+        // 通道的可读事件就绪
+        if (key.isReadable()) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            buffer.clear(); // 清空缓冲区
+            // 读取数据
+            int len = 0;
+            while ((len = socketChannel.read(buffer)) > 0) {
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    System.out.println("Server读取的数据:" + new String(buffer.array(), 0, len));
+                }
+            }
+            if (len < 0) {
+                // 非法的SelectionKey 关闭Channel
+                socketChannel.close();
+            }
+            // SocketChannel通道的可写事件注册到Selector中
+            registerChannel(selector, socketChannel, SelectionKey.OP_WRITE);
+        }
+        // 通道的可写事件就绪
+        if (key.isWritable()) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            buffer.clear(); // 清空缓冲区
+            // 准备发送的数据
+            String message_from_server = "Hello,Client... " + socketChannel.getLocalAddress();
+            buffer.put(message_from_server.getBytes());
+            buffer.flip();
+            socketChannel.write(buffer);
+            System.out.println("Server发送的数据:" + message_from_server);
+            // SocketChannel通道的可写事件注册到Selector中
+            registerChannel(selector, socketChannel, SelectionKey.OP_READ);
         }
 
     }
+
+    // 注册通道到指定Selector上
+    private void registerChannel(Selector selector, SelectableChannel channel, int ops) throws IOException {
+        if (channel == null) {
+            return;
+        }
+        channel.configureBlocking(false);
+        // 注册通道
+        channel.register(selector, ops);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        NIOServer server = new NIOServer();
+        server.init(8888);
+        server.listen();
+    }
+
+
 
 }
